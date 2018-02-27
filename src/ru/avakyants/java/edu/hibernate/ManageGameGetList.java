6a46f54888431d5001d87e1e2125493389f10a09
tema14 <tema14@172.20.10.2>
2018-02-27 16:36:36 +0300

    get object from db, update and save after example

--- src/ru/avakyants/java/edu/hibernate/ManageGameGetList.java
+++ src/ru/avakyants/java/edu/hibernate/ManageGameGetList.java
@@ -8,6 +8,7 @@ import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import ru.avakyants.java.edu.hibernate.model.Game;
 
@@ -16,14 +17,19 @@ public class ManageGameGetList {
 	public static void main(String[] args) {
 		SessionFactory factory = HibernateUtil.getSessionFactory();
 		Session session= factory.openSession();
+		Transaction tx = session.beginTransaction();	
 		
 		CriteriaBuilder builder = session.getCriteriaBuilder();
 		CriteriaQuery<Game> criteria = builder.createQuery(Game.class);
 		//criteria.from(Game.class);
 		Root<Game> rootGame = criteria.from(Game.class);
-		criteria.where(builder.gt(rootGame.get("id"), 1L));
+		//criteria.where(builder.gt(rootGame.get("id"), 1L));
+		criteria.where(builder.equal(rootGame.get("name"), "CSKA - KHIMKI"));
 		
 		List<Game> games = session.createQuery(criteria).getResultList();
+		
+		games.forEach((g)->{ g.setScoreHome((short)17); session.merge(g);});
+		tx.commit();
 		System.out.println(games.size());
 		
 		session.close();
